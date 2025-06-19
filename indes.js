54571a2d16e82e2b6d3c1f6cf462c3d43c6d2083
Lucas-Santos-dev <lucassantosrocha889@gmail.com>
2025-06-19 19:19:38 -0300

    first commit

---
+++ indes.js
@@ -0,0 +1,11 @@
+const customerName = "Lucas";
+const customerCPF = 9999999999;
+const customerAgency = 1001;
+const customerBalance = 0;
+
+const customer1Name = "Lucas";
+const customer1CPF = 9999999999;
+const customer1Agency = 1001;
+const customer1Balance = 0;
+
+console.log(customerName);
